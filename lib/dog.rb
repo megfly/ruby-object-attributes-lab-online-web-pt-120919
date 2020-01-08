1efb66599b3ae425aea9816c0b2b25ba460e9cf1
Meghan Flynn <mflynn333@gmail.com>
2020-01-08 23:57:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,13 @@
 class Dog 
   
+<<<<<<< HEAD
+=======
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end 
+  
+>>>>>>> 24d97444fb21018df52b23a192d3d6a4c11bb5a4
   def name=(name) 
     @name = name 
   end 
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,13 @@
 class Person 
+<<<<<<< HEAD
 
+=======
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end 
+  
+>>>>>>> 24d97444fb21018df52b23a192d3d6a4c11bb5a4
   def name=(name)
     @name = name 
   end 
